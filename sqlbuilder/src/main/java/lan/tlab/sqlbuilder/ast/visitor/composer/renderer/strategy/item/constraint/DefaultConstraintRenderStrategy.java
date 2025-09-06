d2c61c96e60a1d64ce1d48284b6ecdafbb2037f3
auspis <auspis@proton.me>
2025-09-06 08:16:55 +0200

    feat: add DefaultConstraintRenderStrategy for SQL rendering

---
+++ sqlbuilder/src/main/java/lan/tlab/sqlbuilder/ast/visitor/composer/renderer/strategy/item/constraint/DefaultConstraintRenderStrategy.java
@@ -0,0 +1,11 @@
+package lan.tlab.sqlbuilder.ast.visitor.composer.renderer.strategy.item.constraint;
+
+import lan.tlab.sqlbuilder.ast.expression.item.ddl.Constraint.DefaultConstraint;
+import lan.tlab.sqlbuilder.ast.visitor.composer.renderer.SqlRenderer;
+import lan.tlab.sqlbuilder.ast.visitor.composer.renderer.strategy.item.SqlItemRenderStrategy;
+
+public class DefaultConstraintRenderStrategy implements SqlItemRenderStrategy {
+    public String render(DefaultConstraint constraint, SqlRenderer sqlRenderer) {
+        return String.format("DEFAULT %s", constraint.getValue().accept(sqlRenderer));
+    }
+}
