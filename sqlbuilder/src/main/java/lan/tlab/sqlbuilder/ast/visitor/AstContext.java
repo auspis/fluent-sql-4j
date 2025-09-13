bb3bd3af69825d0b764c487e050c7d7df7e2c9e0
massi <massimiliano.p+dev@gmail.com>
2025-09-13 17:15:52 +0200

    feat: add AstContext class for AST visitors to support future context passing

---
+++ sqlbuilder/src/main/java/lan/tlab/sqlbuilder/ast/visitor/AstContext.java
@@ -0,0 +1,8 @@
+package lan.tlab.sqlbuilder.ast.visitor;
+
+/**
+ * Context object for AST visitors. Can be extended in the future for advanced context passing.
+ */
+public class AstContext {
+    // Currently empty; can be extended as needed.
+}
