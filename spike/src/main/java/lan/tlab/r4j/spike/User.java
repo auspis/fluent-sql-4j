9038bdf870d6c40bcc4cbafae0eb09bcd2cbc7d9
auspis <auspis@proton.me>
2025-09-13 12:01:11 +0200

    checkpoint

---
+++ spike/src/main/java/lan/tlab/r4j/spike/User.java
@@ -0,0 +1,3 @@
+package lan.tlab.r4j.spike;
+
+public record User(Integer id, String name, String email) {}
---
+++ spike/src/main/java/lan/tlab/r4j/spike/util/InsertStatementBuilderFromObject.java
@@ -0,0 +1,37 @@
+package lan.tlab.r4j.spike.util;
+
+import java.lang.reflect.RecordComponent;
+import java.util.ArrayList;
+import java.util.List;
+import lan.tlab.sqlbuilder.ast.expression.Expression;
+import lan.tlab.sqlbuilder.ast.expression.item.InsertData.InsertValues;
+import lan.tlab.sqlbuilder.ast.expression.item.Table;
+import lan.tlab.sqlbuilder.ast.expression.scalar.ColumnReference;
+import lan.tlab.sqlbuilder.ast.expression.scalar.Literal;
+import lan.tlab.sqlbuilder.ast.statement.InsertStatement;
+
+public class InsertStatementBuilderFromObject {
+    public static InsertStatement fromObject(String tableName, Object obj) {
+        Class<?> clazz = obj.getClass();
+        List<ColumnReference> columns = new ArrayList<>();
+        List<Expression> values = new ArrayList<>();
+        if (clazz.isRecord()) {
+            for (RecordComponent rc : clazz.getRecordComponents()) {
+                columns.add(ColumnReference.of("", rc.getName()));
+                try {
+                    Object value = rc.getAccessor().invoke(obj);
+                    values.add(Literal.of(value.toString()));
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        } else {
+            throw new IllegalArgumentException("Only records are supported in this version");
+        }
+        return InsertStatement.builder()
+                .table(new Table(tableName))
+                .columns(columns)
+                .data(new InsertValues(values))
+                .build();
+    }
+}
