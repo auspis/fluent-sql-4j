809e34fd03edc42d918bcb26adc24185f8ae5b80
massi <massimiliano.p+dev@gmail.com>
2025-09-20 18:13:03 +0200

    Update DefaultScalarExpressionProjectionPsStrategy to use EscapeStrategy from PreparedStatementVisitor

--- sqlbuilder/src/main/java/lan/tlab/sqlbuilder/ast/visitor/ps/strategy/dialiect/sql2008/DefaultScalarExpressionProjectionPsStrategy.java
+++ sqlbuilder/src/main/java/lan/tlab/sqlbuilder/ast/visitor/ps/strategy/dialiect/sql2008/DefaultScalarExpressionProjectionPsStrategy.java
@@ -5,12 +5,19 @@ import lan.tlab.sqlbuilder.ast.clause.selection.projection.ScalarExpressionProje
 import lan.tlab.sqlbuilder.ast.expression.Expression;
 import lan.tlab.sqlbuilder.ast.visitor.AstContext;
 import lan.tlab.sqlbuilder.ast.visitor.Visitor;
+import lan.tlab.sqlbuilder.ast.visitor.ps.PreparedStatementVisitor;
 import lan.tlab.sqlbuilder.ast.visitor.ps.PsDto;
 import lan.tlab.sqlbuilder.ast.visitor.ps.strategy.ScalarExpressionProjectionPsStrategy;
+import lan.tlab.sqlbuilder.ast.visitor.sql.strategy.escape.EscapeStrategy;
 
 public class DefaultScalarExpressionProjectionPsStrategy implements ScalarExpressionProjectionPsStrategy {
     @Override
     public PsDto handle(ScalarExpressionProjection scalarExpressionProjection, Visitor<PsDto> visitor, AstContext ctx) {
+        EscapeStrategy escapeStrategy = EscapeStrategy.standard();
+        if (visitor instanceof PreparedStatementVisitor psVisitor) {
+            escapeStrategy = psVisitor.getEscapeStrategy();
+        }
+
         Expression expr = scalarExpressionProjection.getExpression();
         String alias = scalarExpressionProjection.getAs().getName();
 
@@ -18,7 +25,7 @@ public class DefaultScalarExpressionProjectionPsStrategy implements ScalarExpres
         String sql = exprResult.sql();
 
         if (!alias.isBlank()) {
-            sql += " AS \"" + alias + "\"";
+            sql += " AS " + escapeStrategy.apply(alias);
         }
         return new PsDto(sql, List.of());
     }
