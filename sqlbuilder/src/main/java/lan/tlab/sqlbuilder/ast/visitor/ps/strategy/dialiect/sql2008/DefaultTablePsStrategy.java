fd8d5a16b8624965a6f981f63a78b50d00eb5d7e
massi <massimiliano.p+dev@gmail.com>
2025-09-20 18:00:46 +0200

    Update DefaultTablePsStrategy to use EscapeStrategy from PreparedStatementVisitor

--- sqlbuilder/src/main/java/lan/tlab/sqlbuilder/ast/visitor/ps/strategy/dialiect/sql2008/DefaultTablePsStrategy.java
+++ sqlbuilder/src/main/java/lan/tlab/sqlbuilder/ast/visitor/ps/strategy/dialiect/sql2008/DefaultTablePsStrategy.java
@@ -4,13 +4,15 @@ import java.util.List;
 import lan.tlab.sqlbuilder.ast.expression.item.Table;
 import lan.tlab.sqlbuilder.ast.visitor.AstContext;
 import lan.tlab.sqlbuilder.ast.visitor.Visitor;
+import lan.tlab.sqlbuilder.ast.visitor.ps.PreparedStatementVisitor;
 import lan.tlab.sqlbuilder.ast.visitor.ps.PsDto;
 import lan.tlab.sqlbuilder.ast.visitor.ps.strategy.TablePsStrategy;
 
 public class DefaultTablePsStrategy implements TablePsStrategy {
     @Override
     public PsDto handle(Table table, Visitor<PsDto> visitor, AstContext ctx) {
-        String sql = "\"" + table.getName() + "\"";
+        PreparedStatementVisitor psVisitor = (PreparedStatementVisitor) visitor;
+        String sql = psVisitor.getEscapeStrategy().apply(table.getName());
         String alias = table.getAs() != null ? table.getAs().getName() : null;
         if (alias != null && !alias.isBlank()) {
             sql += " AS " + alias;
