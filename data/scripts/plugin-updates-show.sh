01e3a49bf7e54810250dfc94765892916e6d51e0
massi <massimiliano.p+dev@gmail.com>
2025-09-20 13:53:39 +0200

    feat: update Maven plugin versions and script for displaying updates

--- data/scripts/plugin-updates-show.sh
+++ data/scripts/plugin-updates-show.sh
@@ -4,7 +4,7 @@
 set -e
 
 # Filtro gli aggiornamenti dei plugin mostrando solo versioni stabili ed eliminando i duplicati
-./mvnw org.codehaus.mojo:versions-maven-plugin:2.16.2:display-plugin-updates -DallowSnapshots=false -Dversions.displayAllVersions=true \
+./mvnw versions:display-plugin-updates -DallowSnapshots=false -Dversions.displayAllVersions=true \
   | grep -e '->' \
   | awk 'BEGIN{IGNORECASE=1} {split($0, a, "-> "); if (a[2] !~ /-(M|RC|beta|alpha|cr|preview|ea)/) print $0}' \
   | sort -u \
