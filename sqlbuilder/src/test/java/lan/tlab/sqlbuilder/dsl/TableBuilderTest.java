fe923aa03209e2a95869f279d871016251bae855
auspis <auspis@proton.me>
2025-09-27 14:20:10 +0200

    feat: implement SelectBuilder and TableBuilder with comprehensive query capabilities and tests

--- sqlbuilder/src/test/java/lan/tlab/sqlbuilder/dsl/TableBuilderTest.java
+++ sqlbuilder/src/test/java/lan/tlab/sqlbuilder/dsl/TableBuilderTest.java
@@ -5,7 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.Test;
 
-class DSLTest {
+class TableBuilderTest {
 
     @Test
     void createUserTable() {
