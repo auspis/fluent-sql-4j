20651a6b42884102b88de9744df517d474ce6d45
massi <massimiliano.p+dev@gmail.com>
2025-10-01 06:00:56 +0200

    refactor(SelectBuilder, DSL): integrate SqlRenderer into SelectBuilder for consistent rendering across methods

--- sqlbuilder/src/main/java/lan/tlab/sqlbuilder/dsl/DSL.java
+++ sqlbuilder/src/main/java/lan/tlab/sqlbuilder/dsl/DSL.java
@@ -1,21 +1,22 @@
 package lan.tlab.sqlbuilder.dsl;
 
 import lan.tlab.sqlbuilder.ast.visitor.sql.SqlRenderer;
+import lan.tlab.sqlbuilder.ast.visitor.sql.factory.SqlRendererFactory;
 import lan.tlab.sqlbuilder.dsl.select.SelectBuilder;
 
 public class DSL {
 
-    static final SqlRenderer SQL_RENDERER = SqlRenderer.builder().build();
+    static final SqlRenderer SQL_RENDERER = SqlRendererFactory.standardSql2008();
 
     public static TableBuilder createTable(String tableName) {
         return new TableBuilder(tableName);
     }
 
     public static SelectBuilder select(String... columns) {
-        return new SelectBuilder(columns);
+        return new SelectBuilder(SQL_RENDERER, columns);
     }
 
     public static SelectBuilder selectAll() {
-        return new SelectBuilder("*");
+        return new SelectBuilder(SQL_RENDERER, "*");
     }
 }
