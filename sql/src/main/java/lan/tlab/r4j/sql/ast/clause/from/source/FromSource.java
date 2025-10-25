815249ed1f7329a86ea1f6923e60704c294b4c92
auspis <auspis@proton.me>
2025-10-25 09:06:47 +0200

    refactor: replace MergeUsing and FromSubquery implementations with records and enhance AliasedTableExpression handling

--- sql/src/main/java/lan/tlab/r4j/sql/ast/clause/from/source/FromSource.java
+++ sql/src/main/java/lan/tlab/r4j/sql/ast/clause/from/source/FromSource.java
@@ -2,4 +2,8 @@ package lan.tlab.r4j.sql.ast.clause.from.source;
 
 import lan.tlab.r4j.sql.ast.visitor.Visitable;
 
+/**
+ * Marker interface for table sources that can be used in FROM clause.
+ * Permitted implementations: TableIdentifier, FromSubquery, OnJoin
+ */
 public interface FromSource extends Visitable {}
