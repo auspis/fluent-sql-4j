57e37b4f4c7e527d421e6ad33627cc7274d8b2d9
massi <massimiliano.p+dev@gmail.com>
2025-10-25 07:03:05 +0200

    refactor(tests): simplify DSL type declaration in insert builder tests

--- sql/src/test/java/lan/tlab/r4j/sql/dsl/insert/InsertBuilderSetMethodTest.java
+++ sql/src/test/java/lan/tlab/r4j/sql/dsl/insert/InsertBuilderSetMethodTest.java
@@ -4,13 +4,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import java.time.LocalDate;
+import lan.tlab.r4j.sql.dsl.DSL;
 import lan.tlab.r4j.sql.test.TestDialectRendererFactory;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class InsertBuilderSetMethodTest {
 
-    private lan.tlab.r4j.sql.dsl.DSL dsl;
+    private DSL dsl;
 
     @BeforeEach
     void setUp() {
