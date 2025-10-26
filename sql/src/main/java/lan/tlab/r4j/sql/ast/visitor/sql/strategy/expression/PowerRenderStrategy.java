c10899066d466faae20122053388f19b8f447b06
massi <massimiliano.p+dev@gmail.com>
2025-10-26 21:34:43 +0100

    refactor: convert Power and DataType classes to records

--- sql/src/main/java/lan/tlab/r4j/sql/ast/visitor/sql/strategy/expression/PowerRenderStrategy.java
+++ sql/src/main/java/lan/tlab/r4j/sql/ast/visitor/sql/strategy/expression/PowerRenderStrategy.java
@@ -9,7 +9,7 @@ public class PowerRenderStrategy implements ExpressionRenderStrategy {
     public String render(Power functionCall, SqlRenderer sqlRenderer, AstContext ctx) {
         return String.format(
                 "POWER(%s, %s)",
-                functionCall.getBase().accept(sqlRenderer, ctx),
-                functionCall.getExponent().accept(sqlRenderer, ctx));
+                functionCall.base().accept(sqlRenderer, ctx),
+                functionCall.exponent().accept(sqlRenderer, ctx));
     }
 }
