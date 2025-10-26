4bff6321ad9f19e29b0f8baee634f08ec561ae99
massi <massimiliano.p+dev@gmail.com>
2025-10-26 12:15:26 +0100

    Convert NullSetExpression to Java record
    - Converted NullSetExpression from class to Java record
    - Record has no components as the class had no fields
    - All existing instantiations remain compatible
    - Tests pass: 1093 tests run, 0 failures

--- sql/src/main/java/lan/tlab/r4j/sql/ast/expression/set/NullSetExpression.java
+++ sql/src/main/java/lan/tlab/r4j/sql/ast/expression/set/NullSetExpression.java
@@ -3,7 +3,7 @@ package lan.tlab.r4j.sql.ast.expression.set;
 import lan.tlab.r4j.sql.ast.visitor.AstContext;
 import lan.tlab.r4j.sql.ast.visitor.Visitor;
 
-public class NullSetExpression implements SetExpression {
+public record NullSetExpression() implements SetExpression {
 
     @Override
     public <T> T accept(Visitor<T> visitor, AstContext ctx) {
