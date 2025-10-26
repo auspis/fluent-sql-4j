7b8e996f1981db3773a2963a75621ee30c513944
auspis <auspis@proton.me>
2025-10-26 11:40:24 +0100

    Convert In to Java record

--- sql/src/main/java/lan/tlab/r4j/sql/ast/predicate/In.java
+++ sql/src/main/java/lan/tlab/r4j/sql/ast/predicate/In.java
@@ -5,15 +5,8 @@ import java.util.stream.Stream;
 import lan.tlab.r4j.sql.ast.expression.Expression;
 import lan.tlab.r4j.sql.ast.visitor.AstContext;
 import lan.tlab.r4j.sql.ast.visitor.Visitor;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
 
-@AllArgsConstructor
-@Getter
-public class In implements Predicate {
-
-    private final Expression expression;
-    private final List<Expression> values;
+public record In(Expression expression, List<Expression> values) implements Predicate {
 
     public In(Expression expression, Expression... values) {
         this(expression, Stream.of(values).toList());
