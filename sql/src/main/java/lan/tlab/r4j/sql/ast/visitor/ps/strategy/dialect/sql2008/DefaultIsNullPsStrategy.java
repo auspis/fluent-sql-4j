175df81b5838c5c3ca127f42149df7f0c26593c4
auspis <auspis@proton.me>
2025-10-26 11:02:13 +0100

    Convert IsNull to Java record

--- sql/src/main/java/lan/tlab/r4j/sql/ast/visitor/ps/strategy/dialect/sql2008/DefaultIsNullPsStrategy.java
+++ sql/src/main/java/lan/tlab/r4j/sql/ast/visitor/ps/strategy/dialect/sql2008/DefaultIsNullPsStrategy.java
@@ -9,7 +9,7 @@ import lan.tlab.r4j.sql.ast.visitor.ps.strategy.IsNullPsStrategy;
 public class DefaultIsNullPsStrategy implements IsNullPsStrategy {
     @Override
     public PsDto handle(IsNull isNull, Visitor<PsDto> renderer, AstContext ctx) {
-        PsDto inner = isNull.getExpression().accept(renderer, ctx);
+        PsDto inner = isNull.expression().accept(renderer, ctx);
         return new PsDto(inner.sql() + " IS NULL", inner.parameters());
     }
 }
