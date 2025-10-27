aca4c20b0df28a22c90894fef7b7f94bc525ddcf
massi <massimiliano.p+dev@gmail.com>
2025-10-27 06:01:54 +0100

    Convert UnaryString and DateArithmetic back to traditional classes
    - UnaryString: record -> final class with private constructor
    - DateArithmetic: record -> final class with private constructor
    - Reason: enforce factory method usage, prevent direct instantiation
    - All tests pass, API unchanged

--- sql/src/main/java/lan/tlab/r4j/sql/ast/expression/scalar/call/function/datetime/DateArithmetic.java
+++ sql/src/main/java/lan/tlab/r4j/sql/ast/expression/scalar/call/function/datetime/DateArithmetic.java
@@ -5,10 +5,18 @@ import lan.tlab.r4j.sql.ast.expression.scalar.call.function.FunctionCall;
 import lan.tlab.r4j.sql.ast.expression.scalar.call.function.datetime.interval.Interval;
 import lan.tlab.r4j.sql.ast.visitor.AstContext;
 import lan.tlab.r4j.sql.ast.visitor.Visitor;
-// TODO: evaluate if the record is the correct solution: the creation should go only through factory methods. see
-// UnaryString.java
 
-public record DateArithmetic(boolean add, ScalarExpression dateExpression, Interval interval) implements FunctionCall {
+public final class DateArithmetic implements FunctionCall {
+
+    private final boolean add;
+    private final ScalarExpression dateExpression;
+    private final Interval interval;
+
+    private DateArithmetic(boolean add, ScalarExpression dateExpression, Interval interval) {
+        this.add = add;
+        this.dateExpression = dateExpression;
+        this.interval = interval;
+    }
 
     public static DateArithmetic add(ScalarExpression dateExpression, Interval interval) {
         return new DateArithmetic(true, dateExpression, interval);
@@ -18,8 +26,41 @@ public record DateArithmetic(boolean add, ScalarExpression dateExpression, Inter
         return new DateArithmetic(false, dateExpression, interval);
     }
 
+    public boolean add() {
+        return add;
+    }
+
+    public ScalarExpression dateExpression() {
+        return dateExpression;
+    }
+
+    public Interval interval() {
+        return interval;
+    }
+
     @Override
     public <T> T accept(Visitor<T> visitor, AstContext ctx) {
         return visitor.visit(this, ctx);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        DateArithmetic that = (DateArithmetic) obj;
+        return add == that.add && dateExpression.equals(that.dateExpression) && interval.equals(that.interval);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Boolean.hashCode(add);
+        result = result * 31 + dateExpression.hashCode();
+        result = result * 31 + interval.hashCode();
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "DateArithmetic[add=" + add + ", dateExpression=" + dateExpression + ", interval=" + interval + "]";
     }
 }
--- sql/src/main/java/lan/tlab/r4j/sql/ast/expression/scalar/call/function/string/UnaryString.java
+++ sql/src/main/java/lan/tlab/r4j/sql/ast/expression/scalar/call/function/string/UnaryString.java
@@ -5,14 +5,15 @@ import lan.tlab.r4j.sql.ast.expression.scalar.call.function.FunctionCall;
 import lan.tlab.r4j.sql.ast.visitor.AstContext;
 import lan.tlab.r4j.sql.ast.visitor.Visitor;
 
-public record UnaryString(String functionName, ScalarExpression expression) implements FunctionCall {
-    // TODO: evaluate if the record is the correct solution: the creation should go only through factory methods. see
-    // DateArithmetic.java
+public final class UnaryString implements FunctionCall {
 
-    // private UnaryString(String functionName, ScalarExpression expression) {
-    //     this.functionName = functionName;
-    //     this.expression = expression;
-    // }
+    private final String functionName;
+    private final ScalarExpression expression;
+
+    private UnaryString(String functionName, ScalarExpression expression) {
+        this.functionName = functionName;
+        this.expression = expression;
+    }
 
     public static UnaryString lower(ScalarExpression stringExpression) {
         return new UnaryString("LOWER", stringExpression);
@@ -22,8 +23,34 @@ public record UnaryString(String functionName, ScalarExpression expression) impl
         return new UnaryString("UPPER", stringExpression);
     }
 
+    public String functionName() {
+        return functionName;
+    }
+
+    public ScalarExpression expression() {
+        return expression;
+    }
+
     @Override
     public <T> T accept(Visitor<T> visitor, AstContext ctx) {
         return visitor.visit(this, ctx);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        UnaryString that = (UnaryString) obj;
+        return functionName.equals(that.functionName) && expression.equals(that.expression);
+    }
+
+    @Override
+    public int hashCode() {
+        return functionName.hashCode() * 31 + expression.hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "UnaryString[functionName=" + functionName + ", expression=" + expression + "]";
     }
 }
