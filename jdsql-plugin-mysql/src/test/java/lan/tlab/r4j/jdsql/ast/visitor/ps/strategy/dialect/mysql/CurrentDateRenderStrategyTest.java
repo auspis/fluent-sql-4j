aa4b39ef30f70401f1fbe547b1423346aa066d82
auspis <auspis@proton.me>
2025-11-15 18:10:03 +0100

    refactor: extracted mysql plugin

--- jdsql-plugin-mysql/src/test/java/lan/tlab/r4j/jdsql/ast/visitor/ps/strategy/dialect/mysql/CurrentDateRenderStrategyTest.java
+++ jdsql-plugin-mysql/src/test/java/lan/tlab/r4j/jdsql/ast/visitor/ps/strategy/dialect/mysql/CurrentDateRenderStrategyTest.java
@@ -1,19 +1,19 @@
-package lan.tlab.r4j.jdsql.ast.visitor.sql.strategy.expression;
+package lan.tlab.r4j.jdsql.ast.visitor.ps.strategy.dialect.mysql;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 import lan.tlab.r4j.jdsql.ast.common.expression.scalar.function.datetime.CurrentDate;
 import lan.tlab.r4j.jdsql.ast.visitor.AstContext;
 import lan.tlab.r4j.jdsql.ast.visitor.sql.SqlRenderer;
-import lan.tlab.r4j.jdsql.plugin.builtin.sql2016.StandardSqlRendererFactory;
+import lan.tlab.r4j.jdsql.plugin.builtin.mysql.MysqlSqlRendererFactory;
 import org.junit.jupiter.api.Test;
 
 class CurrentDateRenderStrategyTest {
 
     @Test
-    void standardSql() {
-        SqlRenderer sqlRenderer = StandardSqlRendererFactory.standardSql();
+    void mysql() {
+        SqlRenderer sqlRenderer = MysqlSqlRendererFactory.mysql();
         String sql = sqlRenderer.visit(new CurrentDate(), new AstContext());
-        assertThat(sql).isEqualTo("CURRENT_DATE()");
+        assertThat(sql).isEqualTo("CURDATE()");
     }
 }
