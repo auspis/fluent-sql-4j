88a1262b2ab31bf65f19f57afc2ec4c1de962b3b
massi <massimiliano.p+dev@gmail.com>
2025-11-22 10:13:45 +0100

    refactor: moved PostgreSQL plugin to dedicated module

--- plugins/jdsql-postgresql/src/test/java/lan/tlab/r4j/jdsql/plugin/builtin/postgre/ast/visitor/sql/strategy/expression/PostgreSqlJsonExistsRenderStrategyTest.java
+++ plugins/jdsql-postgresql/src/test/java/lan/tlab/r4j/jdsql/plugin/builtin/postgre/ast/visitor/sql/strategy/expression/PostgreSqlJsonExistsRenderStrategyTest.java
@@ -1,22 +1,20 @@
 package lan.tlab.r4j.jdsql.plugin.builtin.postgre.ast.visitor.sql.strategy.expression;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import org.junit.jupiter.api.Test;
 import lan.tlab.r4j.jdsql.ast.common.expression.scalar.ColumnReference;
 import lan.tlab.r4j.jdsql.ast.common.expression.scalar.Literal;
 import lan.tlab.r4j.jdsql.ast.common.expression.scalar.function.json.JsonExists;
 import lan.tlab.r4j.jdsql.ast.visitor.AstContext;
 import lan.tlab.r4j.jdsql.ast.visitor.sql.SqlRenderer;
 import lan.tlab.r4j.jdsql.ast.visitor.sql.strategy.expression.JsonExistsRenderStrategy;
-import lan.tlab.r4j.jdsql.plugin.builtin.sql2016.StandardSqlRendererFactory;
-import org.junit.jupiter.api.Test;
+import lan.tlab.r4j.jdsql.plugin.builtin.postgre.PostgreSqlRendererFactory;
 
 class PostgreSqlJsonExistsRenderStrategyTest {
 
-    // TODO: use postgre sql renderer
     @Test
     void ok() {
-        SqlRenderer sqlRenderer = StandardSqlRendererFactory.standardSql();
+        SqlRenderer sqlRenderer = PostgreSqlRendererFactory.create();
         JsonExistsRenderStrategy strategy = new PostgreJsonExistsRenderStrategy();
         JsonExists jsonExists = new JsonExists(ColumnReference.of("products", "data"), Literal.of("$.price"));
         String sql = strategy.render(jsonExists, sqlRenderer, new AstContext());
