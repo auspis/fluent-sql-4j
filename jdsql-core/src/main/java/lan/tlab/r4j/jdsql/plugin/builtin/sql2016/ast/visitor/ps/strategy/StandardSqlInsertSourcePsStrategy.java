91b0fc0a952486e1d046a95d028626ff9aede9ba
auspis <auspis@proton.me>
2025-11-25 04:45:19 +0100

    refactor: update AstContext to use features instead of scope for improved flexibility

--- jdsql-core/src/main/java/lan/tlab/r4j/jdsql/plugin/builtin/sql2016/ast/visitor/ps/strategy/StandardSqlInsertSourcePsStrategy.java
+++ jdsql-core/src/main/java/lan/tlab/r4j/jdsql/plugin/builtin/sql2016/ast/visitor/ps/strategy/StandardSqlInsertSourcePsStrategy.java
@@ -2,7 +2,7 @@ package lan.tlab.r4j.jdsql.plugin.builtin.sql2016.ast.visitor.ps.strategy;
 
 import lan.tlab.r4j.jdsql.ast.dml.component.InsertData.InsertSource;
 import lan.tlab.r4j.jdsql.ast.visitor.AstContext;
-import lan.tlab.r4j.jdsql.ast.visitor.AstContext.Scope;
+import lan.tlab.r4j.jdsql.ast.visitor.AstContext.Feature;
 import lan.tlab.r4j.jdsql.ast.visitor.Visitor;
 import lan.tlab.r4j.jdsql.ast.visitor.ps.PsDto;
 import lan.tlab.r4j.jdsql.ast.visitor.ps.strategy.InsertSourcePsStrategy;
@@ -10,7 +10,7 @@ import lan.tlab.r4j.jdsql.ast.visitor.ps.strategy.InsertSourcePsStrategy;
 public class StandardSqlInsertSourcePsStrategy implements InsertSourcePsStrategy {
     @Override
     public PsDto handle(InsertSource item, Visitor<PsDto> renderer, AstContext ctx) {
-        PsDto psDto = item.setExpression().accept(renderer, new AstContext(Scope.UNION));
+        PsDto psDto = item.setExpression().accept(renderer, new AstContext(Feature.UNION));
         return new PsDto(psDto.sql(), psDto.parameters());
     }
 }
