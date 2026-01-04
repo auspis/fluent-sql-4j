0aebcd233848eb191f145effd699d8afd84ba1eb
auspis <auspis@proton.me>
2026-01-04 18:16:53 +0100

    fix: update Maven plugin versions and improve dependency update script

--- data/scripts/dependency-updates-show.sh
+++ data/scripts/dependency-updates-show.sh
@@ -3,6 +3,14 @@
 
 set -e
 
+# Check if the Maven wrapper script exists
+if [ ! -f "./mvnw" ]; then
+  echo "Maven wrapper script not found! Please ensure you are in the correct directory."
+  exit 1
+fi
+
+echo "\searching for dependency updates (stable versions only)...\n"
+
 ./mvnw versions:display-dependency-updates -DallowSnapshots=false -Dversions.displayAllVersions=true \
   | grep -e '->' \
   | awk 'BEGIN{IGNORECASE=1} {split($0, a, "-> "); if (a[2] !~ /-(M|RC|beta|alpha|cr|preview|ea)/) print $0}' \
